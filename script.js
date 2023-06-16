46a0719aaafa8280b9003acda6eae66be0a7ed67
srimythri1018 <115878123+srimythri1018@users.noreply.github.com>
2023-06-16 15:05:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,53 @@
-const inputs = document.querySelectorAll('.controls input');
+// Get the necessary elements
+const video = document.querySelector('.player__video');
+const progressBar = document.querySelector('.progress__filled');
+const playButton = document.querySelector('.player__button');
+const volumeInput = document.querySelector('input[name="volume"]');
+const playbackSpeedInput = document.querySelector('input[name="playbackRate"]');
+const skipBackButton = document.querySelector('.player__button[data-skip="-10"]');
+const skipForwardButton = document.querySelector('.player__button[data-skip="25"]');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+// Update progress bar as the video plays
+function updateProgressBar() {
+  const progress = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${progress}%`;
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+// Play or pause the video
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+    playButton.textContent = '❚ ❚';
+  } else {
+    video.pause();
+    playButton.textContent = '►';
+  }
+}
+
+// Set the volume of the video
+function setVolume() {
+  video.volume = volumeInput.value;
+}
+
+// Set the playback speed of the video
+function setPlaybackSpeed() {
+  video.playbackRate = playbackSpeedInput.value;
+}
+
+// Skip backward by 10 seconds
+function skipBackward() {
+  video.currentTime -= 10;
+}
+
+// Skip forward by 25 seconds
+function skipForward() {
+  video.currentTime += 25;
+}
+
+// Event listeners
+video.addEventListener('timeupdate', updateProgressBar);
+playButton.addEventListener('click', togglePlay);
+volumeInput.addEventListener('input', setVolume);
+playbackSpeedInput.addEventListener('input', setPlaybackSpeed);
+skipBackButton.addEventListener('click', skipBackward);
+skipForwardButton.addEventListener('click', skipForward);
